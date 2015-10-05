880d207e81b164ca2d02ceba2f312e4415b55fff
alexpchin <alex@alexchin.co.uk>
2015-10-05 10:02:24 +0100

    Adding a little bit of logic

--- js/app2.js
+++ js/app2.js
@@ -23,181 +23,147 @@
   var squares = [], 
       empty = "\xA0",
       score,
-      turn,
-      moveCount,
+      moves,
+      turn = "X",
+      oldOnload,
       numOfPlayers = 1,
       result = document.createElement("h2"),
-
-      // To generate dynamically later
-      wins = [7, 56, 448, 73, 146, 292, 273, 84], 
-      positions = [256,128,64,32,16,8,4,2,1],     
-
-
-      // Don't know if I need these...
+      wins = [7, 56, 448, 73, 146, 292, 273, 84], // To generate dynamically later
+      positions = [256,128,64,32,16,8,4,2,1], // To generate dynamically later   
+      nextMove,
       playerMoves = [],
       computerMoves = [],
-      nextMove,
-
-  MoveList = function() {
-    this.X = [];
-    this.O = [];
-  },
-
-  Scoreboard = function() {
-    this.X = 0;
-    this.O = 0;
-  },
 
   /*
-   * Shallow cloning function
-   */
-  clone = function(obj){
-    return JSON.parse(JSON.stringify(obj));
-  },
-
-  /*
-   * Reset the score, turn and moveCount to the defaults
+   * Reset the score, turn and moves to the defaults
    */
   startNewGame = function () {
     turn = "X";
-    moveCount = 0;
-    score = new Scoreboard();
+    score = {"X": 0, "O": 0};
+    moves = 0;
   },
 
   /*
    * See if the score is higher than the powered score value
    * start from the highest value and moving down
    */
-  cellsFromScore = function(tempscore, tempturn){
-    // var moveList = new MoveList(),
-    //     score,
-    //     p = positions.length,
-    //     cellValue;
+  // cellsFromScore = function(tempscore, tempturn){
+  //   // var moveList = { X: [], O: [] },
+  //   //     score,
+  //   //     p = positions.length,
+  //   //     cellValue;
 
-    // for (i in tempscore) {
-    //   score = tempscore[i];
+  //   // for (i in tempscore) {
+  //   //   score = tempscore[i];
 
-    //   for (p; p >= 0; p--) {
-    //     if (score === 0) { break }
+  //   //   for (p; p >= 0; p--) {
+  //   //     cellValue = Math.pow(2, p);
+  //   //     if (Math.floor(score/Math.pow(2, p)) >= 1) {
+  //   //       moveList[i].push(p);
+  //   //       score -= cellValue;
+  //   //     }
+  //   //   }
+  //   //   p = 0;
+  //   // }
 
-    //     cellValue = Math.pow(2, p);
-    //     if (Math.floor(score/cellValue) > 0) {
-    //       moveList[i].push(p);
-    //       score -= cellValue;
-    //     }
-    //   }
+  //   // return moveList;
 
-    //   p = 0;
-    // }
-    var moveList = [],
-        score,
-        p = positions.length,
-        cellValue;
+  //   var moveList = [],
+  //       remainingScore,
+  //       p = 9,
+  //       cellValue;
 
-    for (i in tempscore) {
-      score = tempscore[i];
+  //   for (i in tempscore) {
+  //     remainingScore = tempscore[i];
 
-      for (p; p >= 0; p--) {
-        if (score === 0) { break }
+  //     for (p; p === 0; p--) {
+  //       cellValue = Math.pow(2, p);
+  //       console.log(cellValue);
+  //       if (Math.floor(remainingScore/cellValue) >= 1) {
+  //         moveList.push(p);
+  //         remainingScore -= cellValue;
+  //       } 
+  //     }
 
-        cellValue = Math.pow(2, p);
-        if (Math.floor(score/cellValue) > 0) {
-          moveList.push(p);
-          score -= cellValue;
-        }
-      }
+  //     debugger
+  //     // Reset for the other player
+  //     remainingScore = 0;
+  //     p = 9;
+  //   }
 
-      p = 0;
-    }
-
-    return moveList;
-  },
+  //   return moveList;
+  // },
 
   /*
-   * Loop through the played cells and return all the unplayed cells
+   * Return all the unplayed cells
    */
-  possibleMoves = function(played){
-    var i = 0,
+  possibleMoves = function(player, computer){
+    var played = player.concat(computer),
+        i = 0,
         unplayed = [];
 
     for(i; i < 9; i++) {
-      // if (played.X.indexOf(i) === -1 && played.O.indexOf(i) === -1) {
       if (played.indexOf(i) === -1) {
         unplayed.push(i);
       }
     }
-
     return unplayed; 
   },
 
   minmaxMoves = [],
   minmaxScores = [],
 
-  /*
-   * A recursive minmax function that looks up the best move
-   */
-  minmax = function(tempscore, previousTurn, pMoves, cMoves, depth) {
-    var played   = cellsFromScore(tempscore, previousTurn),
-        unplayed = possibleMoves(played),
-        nextTurn = (previousTurn === "X" ? "O" : "X"),
+  minmax = function(tempscore, tempturn, pMoves, cMoves, depth) {
+    var possible = possibleMoves(playerMoves, computerMoves),
+        opponent = tempturn === "X" ? "O" : "X",
         i = 0,
         next;
 
-    debugger
-
-    console.log("Unplayed " + unplayed)
-
-    // If the previous turn was a terminal one, return a minmax score
-    if (win(tempscore[previousTurn])) {
+    if (win(tempscore[tempturn])) {
+      console.log("O wins");
       return 10 - depth;
-    } else if (win(tempscore[nextTurn])) {
+    } else if (win(tempscore[opponent])) {
+      console.log("X wins");
       return depth - 10;
-    } else if (unplayed.length === 0) {
+    } else if (possible.length === 0) {
+      console.log("Tie");
       return 0;
     } else {
 
-      // Increase the depth
       depth++;
 
-      // Loop through the unplayed moves
-      for (i; i < unplayed.length; i++){
-        
-        // Duplicate the tempscore & the player & computer moves...
-        // Might not be necessary
-        var tempscore = clone(tempscore);
-        var tempPlayerMoves = clone(pMoves);
-        var tempComputerMoves = clone(cMoves);
+      // console.log(possible);
+      for (i; i < possible.length; i++){
+        next = Math.pow(2, possible[i]);
+        // Add the score next score to the players score
+        var tempscore = JSON.parse(JSON.stringify(tempscore));
+        var tempPlayerMoves = pMoves;
+        var tempComputerMoves = cMoves;
 
-        // Add each unplayed move to the next player's
-        if (nextTurn === "X") {
-          tempPlayerMoves.push(unplayed[i]);
+        if (tempturn === "X") {
+          tempPlayerMoves.push(possible[i]);
         } else {
-          tempComputerMoves.push(unplayed[i]);
+          tempComputerMoves.push(possible[i]);
         }
 
-        // Add the move value to the score.
-        next = Math.pow(2, unplayed[i]);
-        tempscore[nextTurn] += next;
-
-        minmaxMoves.push(unplayed[i]);
-        minmaxScores.push(minmax(tempscore, nextTurn, tempPlayerMoves, tempComputerMoves, depth));
+        tempscore[tempturn] += next;
+        minmaxMoves.push(possible[i]);
+        minmaxScores.push(minmax(tempscore, opponent, tempPlayerMoves, tempComputerMoves, depth));
       }
     }
 
     var maxScore = Math.max.apply(null, minmaxScores);
     var minScore = Math.min.apply(null, minmaxScores);
+
+    console.log(minmaxMoves)
+    console.log(minmaxScores)
+    console.log(maxScore)
+    console.log(minScore)
+
     var maxMove = minmaxMoves[minmaxScores.indexOf(maxScore)];
     var minMove = minmaxMoves[minmaxScores.indexOf(minScore)];
 
-    console.log("minmaxMoves " + minmaxMoves)
-    console.log("minmaxScores " + minmaxScores)
-    // console.log("maxScore " + maxScore)
-    // console.log("minScore " + minScore)
-    // console.log(maxMove)
-    // console.log(minMove)
-
-    return minMove
-    // return minmaxMoves[minmaxScores.indexOf(maxScore)]
+    return minmaxMoves[minmaxScores.indexOf(maxScore)]
   },
 
   /*
@@ -233,40 +199,34 @@
     if (turn === "X") {
       playerMoves.push(squares.indexOf(cell));
     } else {
-      // debugger
       computerMoves.push(squares.indexOf(cell));
     }
 
     cell.firstChild.nodeValue = turn;
     cell.setAttribute("class", turn.toLowerCase());
-    moveCount += 1;
+    moves += 1;
     score[turn] += cell.indicator;
+
+    // Change the player (computer is O)
+    turn = turn === "X" ? "O" : "X";
 
     // Check win
     if (win(score[turn])) {
       result.innerHTML = turn + " wins!";
       startNewGame();
-    } else if (moveCount === 9) {
+    } else if (moves === 9) {
       result.innerHTML = "It's a tie";
       startNewGame();
     } else {
 
-      // Change the player (computer is O)
-      turn = (turn === "X" ? "O" : "X");
-      // console.log(score);
-      // console.log(turn);
-
       // swap turns
       if (numOfPlayers < 2 && turn === "O") {
-
         // Clone the score object
-        var tempscore = clone(score);
-        var tempPlayerMoves = clone(playerMoves);
-        var tempComputerMoves = clone(computerMoves);
+        var tempscore = JSON.parse(JSON.stringify(score));
         var tempturn  = turn;
 
-        var next = minmax(tempscore, tempturn, tempPlayerMoves, tempComputerMoves, 0);
-        // debugger
+        var next = minmax(tempscore, tempturn, playerMoves, computerMoves, 0);
+        
         set(null, squares[next]);
         
         // Reset Minmax containers
